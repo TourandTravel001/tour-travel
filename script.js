f8d87ab543c21205d34e8cfbc2f6cc9a249c4077
TourandTravel001 <zander78945@gmail.com>
2025-10-09 10:47:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function showMessage() {
-  alert("Thanks for choosing Tour & Travel! ✈️ Your adventure starts here!");
+function scrollToPackages() {
+  document.getElementById("packages").scrollIntoView({ behavior: "smooth" });
 }
+
+const buttons = document.querySelectorAll('.book');
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert("Thanks for choosing Tour & Travel! We’ll contact you soon 😊");
+  });
+});
